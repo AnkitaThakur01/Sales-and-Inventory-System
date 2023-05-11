fc13eadcfa51d8550989ac1081d8f145931b6216
ANKITA THAKUR <ankita.at1999@gmail.com>
2023-05-12 02:43:25 +0530

    Initial prod build

---
+++ build/precache-manifest.09dece551428eb0eb2871951aa3b55c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89db17f03ee83fe569c0c6f21366ea2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67e66a61308f4f35b334",
+    "url": "/static/css/2.57754de2.chunk.css"
+  },
+  {
+    "revision": "f8fb5d9f1ed59cd94ce2",
+    "url": "/static/css/main.aa862829.chunk.css"
+  },
+  {
+    "revision": "67e66a61308f4f35b334",
+    "url": "/static/js/2.e6e07974.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6e07974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8fb5d9f1ed59cd94ce2",
+    "url": "/static/js/main.74e81b3f.chunk.js"
+  },
+  {
+    "revision": "d9ca308a650c4e5e39be",
+    "url": "/static/js/runtime-main.32c0e09d.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7f354e93c30f9d51fc3aa5e851918e4c",
+    "url": "/static/media/user-img-background.7f354e93.jpg"
+  }
+]);
